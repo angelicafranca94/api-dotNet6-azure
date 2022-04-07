fb770963a6de7561cf38edb4f1e344218f436194
Angélica França <gefranca94@gmail.com>
2022-04-07 09:44:46 -0300

    Modelando as entidades

---
+++ Manager.API/Manager.Domain/Entities/User.cs
@@ -0,0 +1,60 @@
+﻿using Manager.Domain.Validators;
+
+namespace Manager.Domain.Entities;
+
+public class User : Base
+{
+    //Propriedades
+    public string Name { get; private set; }
+
+    public string Email { get; private set; }
+
+    public string Password { get; private set; }
+
+    //EF
+    protected User() { }
+
+    public User(string name, string email, string password)
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+        _erros = new List<string>();
+    }
+
+    //Comportamentos
+    public void ChangeName(string name)
+    {
+        Name = name;
+        Validate();
+    }
+
+    public void ChangeEmail(string email)
+    {
+        Email = email;
+        Validate();
+    } 
+    
+    public void ChangePassword(string password)
+    {
+        Password = password;
+        Validate();
+    }
+
+    //Autovalida
+    public override bool Validate()
+    {
+        var validator = new UserValidator();
+        var validation = validator.Validate(this);
+
+        if (!validation.IsValid)
+        {
+            foreach (var error in validation.Errors)
+                _erros.Add(error.ErrorMessage);
+
+            throw new Exception($"Campos inválidos {validation.Errors} {_erros[0]}");
+        }
+
+        return true;
+    }
+}
---
+++ Manager.API/Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation;
+using Manager.Domain.Entities;
+
+namespace Manager.Domain.Validators;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(u => u)
+            .NotEmpty()
+            .WithMessage("A entidade não pode ser vazia!")
+
+            .NotNull()
+            .WithMessage("A entidade não pode ser nula!");
+
+        RuleFor(u => u.Name)
+            .NotNull()
+            .WithMessage("Nome não pode ser nulo!")
+
+            .NotEmpty()
+            .WithMessage("Nome não pode ser vazio!")
+
+            .MinimumLength(3)
+            .WithMessage("Nome deve conter no mínimo 3 caracteres")
+
+            .MaximumLength(50)
+            .WithMessage("Nome deve conter no máximo 50 caracteres");
+
+        RuleFor(u => u.Email)
+            .NotNull()
+            .WithMessage("Email não pode ser nulo!")
+
+            .NotEmpty()
+            .WithMessage("Email não pode ser vazio!")
+
+            .MinimumLength(10)
+            .WithMessage("Email deve conter no mínimo 10 caracteres")
+
+            .MaximumLength(50)
+            .WithMessage("Email deve conter no máximo 50 caracteres")
+
+            .Matches(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
+            .WithMessage("O email informado não é válido.");
+
+
+        RuleFor(u => u.Password)
+            .NotNull()
+            .WithMessage("Senha não pode ser nulo!")
+
+            .NotEmpty()
+            .WithMessage("Senha não pode ser vazio!")
+
+            .MinimumLength(6)
+            .WithMessage("Senha deve conter no mínimo 6 caracteres")
+
+            .MaximumLength(20)
+            .WithMessage("Senha deve conter no máximo 20 caracteres"); ;
+    }
+}
